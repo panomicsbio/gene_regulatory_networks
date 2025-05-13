052ab8e06d5bb9814970db2cc2f2ccb7d9a799de
Radu Tanasa <radu@geneguard.bio>
2025-05-13 13:13:24 +0300

    add missing utility function

--- app/utils.py
+++ app/utils.py
@@ -3,3 +3,7 @@ import os
 
 def get_R_script_base_path():
     return f"{os.getcwd()}/../app" if '/tests' in os.getcwd() else f"{os.getcwd()}/app"
+
+
+def get_directory_files(directory):
+    return [os.path.join(directory, f) for f in os.listdir(directory) if os.path.isfile(os.path.join(directory, f))]
